716cb621fc29d30c36f50e666bd7ae504e20e469
Heather Engle <h.a.engle@gmail.com>
2025-02-16 20:08:12 -0500

    project card

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Card } from './Card'
-export { default as ThemeToggle } from './ThemeToggle'
-export { default as Header } from './Header'
-export { default as Nav } from './Nav'
-export { default as Contact } from './Contact'
